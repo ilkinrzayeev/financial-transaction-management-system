aad32df442d172e2b920579b1964220e16918df8
aliagasiyev <agasiyevali2@gmail.com>
2025-07-05 19:12:19 +0400

    Add customer management page with form validation, customer search, and backend API integration in React frontend

--- src/main/java/az/edu/financialtransactionmanagementsystem/config/WebConfig.java
+++ src/main/java/az/edu/financialtransactionmanagementsystem/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*");
+                        .allowedMethods("**");
             }
         };
     }
